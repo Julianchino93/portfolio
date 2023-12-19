93841448754106bc461a0477a195d1d2e34216e2
Julianchino93 <torres_226@hotmail.com>
2023-12-18 21:08:52 -0300

    php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
+<?php 
+$pg ="inicio";
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en" class="h-100">
 <head>
@@ -14,34 +20,7 @@
     
 <body id="inicio">
     <header class="container">
-        <nav class="navbar navbar-expand-md">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                  <li class="nav-item">
-                    <a class="nav-link px-4 active" href="index.html">Inicio</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link px-4" href="proyectos.html">Proyectos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link px-4" href="sobre-mi.html">Sobre mi</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link px-4" href="contacto.html">Contacto</a>
-                  </li>
-                </ul>
-                <ul class="navbar-nav">
-                  <li class="float-sm-end">
-                      <a class="btn btn-rojo" href="contacto.html">Ver mi Curriculum <i class="fa-solid fa-download"></i></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
-    
+    <?php  include_once("menu.php"); ?>
     </header>
     <main class="container">
 <div class="row">
--- proyectos.php
+++ proyectos.php
@@ -1,5 +1,13 @@
-<!DOCTYPE html>
-<html lang="es" class="h-100">
+<?php 
+$pg ="proyectos";
+
+
+?>
+
+
+
+<!DOCTYPE php>
+<php lang="es" class="h-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -14,33 +22,7 @@
 </head>
 <body id="proyectos" class="d-flex flex-column h-100">
     <header class="container">
-        <nav class="navbar navbar-expand-md">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarCollapse">
-                <ul class="navbar-nav me-auto mb-2 mb-md-0">
-                  <li class="nav-item">
-                    <a class="nav-link px-4" href="index.html">Inicio</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link px-4 active" href="proyectos.html">Proyectos</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link px-4" href="sobre-mi.html">Sobre mi</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link px-4" href="contacto.html">Contacto</a>
-                  </li>
-                </ul>
-                <ul class="navbar-nav">
-                  <li class="float-sm-end">
-                      <a class="btn btn-rojo" href="contacto.html">Ver mi Curriculum <i class="fa-solid fa-download"></i></a>
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </nav>
+    <?php include_once("menu.php"); ?>
     </header>
     <main class="container">
       <div class="row">
